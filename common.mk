a8452685c9b7b0d1a58a809da2db89dfe65945d7
RedSTARO <822858602@qq.com>
2025-07-11 12:59:55 +0800

    sm8350: Use AOSP IR service

--- common.mk
+++ common.mk
@@ -231,9 +231,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.example
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
-
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := false
 
